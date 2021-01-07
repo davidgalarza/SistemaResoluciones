69024e2af5cc18fcb63e55e245784be794bdd327
David Galarza <vatayo14@gmail.com>
2021-01-07 00:28:41 -0500

    User seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $adminRole = Role::create([
+            'nombre' => 'ADMINISTRADOR',
+            'descripcion' => 'ADMINISTRADOR'
+        ]);
+
+        $abogadoRole = Role::create([
+            'nombre' => 'ABOGADO',
+            'descripcion' => 'ABOGADO'
+        ]);
+
+        $user = User::create([
+            'name' => 'Administrador',
+            'email' => 'administrador'.'@fisei.com',
+            'password' => Hash::make('12345678')
+        ]);
+
+        $user->asignarRol($adminRole);
+
+        $user = User::create([
+            'name' => 'Abogado',
+            'email' => 'abogado'.'@fisei.com',
+            'password' => Hash::make('12345678')
+        ]);
+        
+
+
+        $user->asignarRol($abogadoRole);
+        
+    }
+}
