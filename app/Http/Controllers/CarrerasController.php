dec704c3058f8c5ef78f215ff4e45dfc85279158
DendroM06 <66705082+DendroM06@users.noreply.github.com>
2021-01-16 22:47:01 -0500

    Update CarrerasController.php

--- app/Http/Controllers/CarrerasController.php
+++ app/Http/Controllers/CarrerasController.php
@@ -6,42 +6,89 @@ use App\Carrera;
 use App\Http\Requests\CarreraFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
 
 class CarrerasController extends Controller
 {
-    public function index() {
-        $sql = "SELECT * FROM carreras WHERE eliminado = 0"; 
+    public function index(Request $request)
+    {
+        //$sql = "SELECT * FROM carreras WHERE eliminado = 0";
         // $carreras = Carrera::Search('eliminado'->'1');
         // $carreras = Carrera::all();
-        $carreras = DB::select($sql);
+        // $carreras = DB::select($sql);
+        if ($request) {
+            # code...            
+            $query = trim($request->get('search'));
+            $carreras = Carrera::where('nombre', 'LIKE', '%' . $query . '%')
+            ->where('eliminado','=',0)
+            ->orderBy('id','asc')
+            ->get();
+            return view('carreras.index', ['carreras' => $carreras, 'search' => $query]);
+        }
 
         //return view('carreras.index');
-        return view('carreras.index',['carreras' => $carreras]);
+        
     }
 
     //Agregar carrera
-    public function create() {        
+    public function create()
+    {
         return view('carreras.nuevo');
     }
 
-    public function store(CarreraFormRequest $request){            
+    public function store(CarreraFormRequest $request)
+    {
+
+        $carrera = new Carrera();
+        //$sql = "SELECT eliminado FROM carreras WHERE nombre = " . request('nombre');
+        try {
+            //code...
+           $eliminados = Carrera::where('nombre', '=', request('nombre'))
+           ->orderBy('id','asc')
+           ->get();
+           //print_r($eliminados);
+           if ($eliminados[0]->eliminado == '1' && count($eliminados)==1) {
+            $carrera->nombre = request('nombre');
+            $carrera->save();
+            return redirect('/carreras');
+        } else if ($eliminados[0]->eliminado == '0' || count($eliminados)>1) {
+
+            $messages = [
+                'nombre.unique' => 'El nombre de la carrera ya está en uso.',
+            ];
+            $validator = Validator::make($request->all(), [
+                'nombre' => 'unique:carreras',
+            ],$messages);
+            if ($validator->fails()) {
+                return redirect('/carreras/nuevo')
+                            ->withErrors($validator)
+                            ->withInput();
+            }
+        }
+        } catch (\Throwable $th) {
+            # code...   
+            $carrera->nombre = request('nombre');
+            $carrera->save();    
+            //print_r($eliminados);
+            return redirect('/carreras');  
+        } 
         
-        $carrera= new Carrera();
-        $carrera->nombre = request('nombre');
-        $carrera->save();
+        // $carrera->nombre = request('nombre');
+        // $carrera->save();
         //return view('carreras.nuevo');
-        return redirect('/carreras');
     }
 
     public function edit($id)
     {
-        
+
         return view('carreras.edit', ['carrera' => Carrera::findOrFail($id)]);
     }
 
 
-    public function update(CarreraFormRequest $request, $id){
-        $carrera=  Carrera::findOrFail($id);
+    public function update(CarreraFormRequest $request, $id)
+    {
+        $carrera =  Carrera::findOrFail($id);
         $carrera->nombre = $request->get('nombre');
         $carrera->update();
         //return view('carreras.nuevo');
@@ -51,12 +98,11 @@ class CarrerasController extends Controller
     public  function destroy($id)
     {
         # code...
-        $carrera=Carrera::findOrFail($id);
+        $carrera = Carrera::findOrFail($id);
         //$carrera->delete();
 
         $carrera->eliminado = 1;
         $carrera->update();
         return redirect('/carreras');
     }
-
 }
